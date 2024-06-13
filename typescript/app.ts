aa33decb71651f5a7f6d046ec89bb93ea2a730e7
siddesh0611 <siddesh0601@gmail.com>
2024-06-13 12:10:49 +0530

    post

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import todosRoutes from './routes/todos';
+import { textSpanContainsPosition } from 'typescript';
 
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todosRoutes);
+
+app.listen(3000);
--- typescript/dist/routes/todos.js
+++ typescript/dist/routes/todos.js
@@ -1,28 +1,19 @@
-import express, { Router } from 'express';
-
-import { Todo } from '../models/todo';
-import { todo } from 'node:test';
-
-let todos: Todo[] = []
-type RequestBody = { text: string };
-type Requestparams = { todoId: string };
-
-const router = Router();
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
-
 router.post('/todo', (req, res, next) => {
-    const body = req.body as RequestBody
-    const newTodo: Todo = {
+    const body = req.body;
+    const newTodo = {
         id: new Date().toISOString(),
         text: body.text
-    }
-
+    };
     todos.push(newTodo);
-})
-
+});
 router.put('/todo/:todoId', (req, res, next) => {
     const tid = req.params.todoId;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
@@ -32,10 +23,8 @@ router.put('/todo/:todoId', (req, res, next) => {
     }
     res.status(404).json({ message: 'could not find' });
 });
-
 router.delete('/todo/:todoId', (req, res, next) => {
     todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
     res.status(400).json({ message: 'Deleted todo', todos: todos });
-})
-
-export default router;
+});
+exports.default = router;
